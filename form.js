5e65be645cd865f69ff1026930e7d80bd4421fc4
Yat <yat@Yat-iMac.local>
2019-04-19 12:09:43 -0600

    Step visibility based on radio option
    Not fully working but getting strange bugs when removing seemingly innocous code (i.e. comments).

--- form.js
+++ form.js
@@ -1,20 +1,18 @@
 "use strict";
 
 // TO LOAD DEFAULT VISIBILITY CONDITIONS (works)
-window.onload = function newsletterCBDefaultState() {
+window.onload = function setDefaultAnswerState() {
   // for newsletter checkbox
-  if (newsletterCheckbox.checked == true) {
-    ticketDate.style.display = "block";
-  } else {
-    ticketDate.style.display = "none";
-    }
+  newsletterCheckbox.checked = false;
   // for parking violation radios
-  if (hoomanYesRadio.checked == true) {
-    typeOfViolation.style.display = "block";
-  } else {
-    typeOfViolation.style.display = "none";
-    }
-  };
+  hoomanYesRadio.checked = true
+
+  // to display only first step until next button clicked
+  buttonVisibility();
+};
+
+// To set default answers/visibility. To be used on page load.
+
 
 // CB VISIBILITY CONDITION (works)
 const newsletterCheckbox = document.getElementById("newsletterSignUp");
@@ -24,7 +22,7 @@ const ticketDate = document.getElementById("ticket-date");
 const newsletterCheckboxVisiblity = (function() {
 newsletterCheckbox.addEventListener("click", checkboxChanged);
 function checkboxChanged() {
-  if (newsletterCheckbox.checked == true) {
+  if (newsletterCheckbox.checked === true) {
     ticketDate.style.display = "block";
   } else {
     ticketDate.style.display = "none";
@@ -42,7 +40,7 @@ const typeOfViolation = document.getElementById("parking-violation-radio");
 const hoomanYNRadioAnswer = (function() {
   ifAHoomanYNRadio.addEventListener("click", radioChangedYN);
   function radioChangedYN() {
-    if (hoomanYesRadio.checked == true) {
+    if (hoomanYesRadio.checked === true) {
       typeOfViolation.style.display = "block";
     } else {
       typeOfViolation.style.display = "none";
@@ -50,43 +48,22 @@ const hoomanYNRadioAnswer = (function() {
     }
 }());
 
-// MULTI RADIO BUTTON VISIBILITY CONDITION
-// From Stack Overflow (works when class added to radio buttons)
-const inputs = document.querySelectorAll(".test-class");
 
-const parkingViolationMULTIRadioAnswer = (function checkRadioAnswer() {
-
-  for(let i = 0; i < inputs.length; i++) {
-      inputs[i].addEventListener("click", checkRadioAnswer, false);
+// To insert text into final output based on selections. May need to change to more specific if statement
+function insertText(answerOption) {
+  if (answerOption === "1") {
+    document.getElementById("insert-text-here").innerHTML = "-insert answer 1 text here-";
   }
-
-  var radios = document.getElementsByName("violationType")
-
-  function insertText() {
-    document.getElementById("insert-text-here").innerHTML = "-insert answer 4 text here-";
-  };
-
-  for (let i = 0, length = radios.length; i < length; i++) {
-    if (radios[i].checked) {
-      if (radios[i].value == "1") {
-        ticketDate.style.display = "none";
-        document.getElementById("insert-text-here").innerHTML = "";
-      }
-      else if (radios[i].value == "2") {
-        ticketDate.style.display = "none";
-      }
-      else if (radios[i].value == "3") {
-        ticketDate.style.display = "block";
-        document.getElementById("insert-text-here").innerHTML = "-insert answer 3 text here-";
-      }
-      else if (radios[i].value == "4") {
-        ticketDate.style.display = "block";
-        insertText();
-      }
-    }
+  else if (answerOption === "2") {
+    document.getElementById("insert-text-here").innerHTML = "-insert answer 2 text here-";
   }
-}());
-
+  else if (answerOption === "3") {
+    document.getElementById("insert-text-here").innerHTML = "-insert answer 3 text here-";
+  }
+  else if (answerOption === "4") {
+    document.getElementById("insert-text-here").innerHTML = "-insert answer 4 text here-"; // for output letter
+  }
+};
 
 /* works
 const showNextQ = (function () {
@@ -115,16 +92,16 @@ const currentStep = (function() {
 */
 
 
-/* Previous/Next/Submit button visiblity and to scroll to next div/step */
+// Previous/Next/Submit button visiblity and to scroll to next div/step. Needs to be initialized before question specific visibility conditions
 const stepsQuestionnaire = document.getElementsByClassName("section-container");
-
-const buttonVisibility = (function() {
+const finalSectionDiv = document.getElementById("final-section-container");
+const buttonVisibility = function() {
   var count = 0;
   function checkButtonStep() {
-    if (count == 0) {
+    if (count === 0) {
       document.getElementById("button-previous").style.display="none";
       document.getElementById("button-submit").style.display="none";
-    } else if (count > 0 && count < stepsQuestionnaire.length -1) {
+    } else if (count > 0 && count < stepsQuestionnaire.length - 1) {
         document.getElementById("button-previous").style.display="inline";
         document.getElementById("button-submit").style.display="none";
     } else if (count >= stepsQuestionnaire.length - 1) {
@@ -136,10 +113,52 @@ const buttonVisibility = (function() {
 
   checkButtonStep();
 
+  // To hide all steps other than first and last section by default
+  const stepHideByDefault = function() {
+    for (var i = 1; i < stepsQuestionnaire.length; i++) {
+      stepsQuestionnaire[i].style.display="none";
+    }
+  };
+  stepHideByDefault();
+
+  // To make next step visible. Used inside onclick for next button
+/*  const stepMakeVisible = function() {
+      stepsQuestionnaire[count].style.display="block";
+      updateStepVisiblity();
+  };
+*/
+// test partially works for single parameter
+/*  const stepMakeVisible = function(stepToHide) { // rest parameter seems right but not working?? What happens if back-to-back hidden sections?
+    if (stepsQuestionnaire[count] == stepToHide) {
+      stepsQuestionnaire[count+1].style.display="block";
+      stepsQuestionnaire[count+1].scrollIntoView(true);
+    } else {
+      stepsQuestionnaire[count].style.display="block";
+    }
+  }; */
+
+  // test 2 works except when use array
+    const stepMakeVisible = function(...stepToHide) { // rest parameter seems right but not working?? What happens if back-to-back hidden sections?
+      console.log(stepToHide);
+      for (let i = 0; i < stepToHide.length; i++) {
+        if (stepsQuestionnaire[count] === stepToHide[i]) { // works when set to ticketDate but not variable ??
+          stepsQuestionnaire[count+1].style.display="block";
+          stepsQuestionnaire[count+1].scrollIntoView(true);
+          console.log("hide a step");
+        } else {
+          stepsQuestionnaire[count].style.display="block";
+          console.log("no step to hide. continue as usual");
+        }
+      }
+    };
+
   document.getElementById("button-next").onclick = function() {
     if (count < stepsQuestionnaire.length - 1) {
       count++;
+      stepMakeVisible(hideTheseAnswersArray);
       stepsQuestionnaire[count].scrollIntoView(true);
+      stepsQuestionnaire[count].style.opacity="1";
+      stepsQuestionnaire[count-1].style.opacity="0.2"; // reduce opacity of a completed step so user focus is on current step
       checkButtonStep();
     } else {
         checkButtonStep();
@@ -151,16 +170,104 @@ const buttonVisibility = (function() {
       checkButtonStep();
     } else if (count >= 1) {
         count--;
+        stepsQuestionnaire[count].style.opacity="1";
+        stepsQuestionnaire[count+1].style.opacity="0.2";
         stepsQuestionnaire[count].scrollIntoView(true);
         checkButtonStep();
     } return count;
   };
+};
 
+// MULTI RADIO BUTTON VISIBILITY CONDITION
+// From Stack Overflow (works when class added to radio buttons)
+const inputs = document.querySelectorAll(".test-class");
+
+const hideTheseAnswersArray = [];
+
+/* function updateHideTheseAnswersArray(addItem, subtractItem) { // does add/subtract need to be separate functions??
+  if (hideTheseAnswersArray.indexOf(addItem) === -1) {
+    hideTheseAnswersArray.push(addItem);
+    console.log("add an item: " + addItem);
+    console.log(addItem);
+  } else if (hideTheseAnswersArray.indexOf(addItem) === 0) {
+      console.log("There's already an " + addItem + " here. Don't do anything");
+  } else if (hideTheseAnswersArray.indexOf(addItem && subtractItem) > -1) {
+      var arrayIndex = hideTheseAnswersArray.indexOf(subtractItem);
+      hideTheseAnswersArray.splice(arrayIndex, 1);
+      console.log("subtract:" + subtractItem);
+      console.log(subtractItem);
+      }
+} */
+
+function updateHideTheseAnswersArray(addItem, subtractItem) {
+  function addToHideTheseAnswersArray(addItem) {
+    if (hideTheseAnswersArray.indexOf(addItem) === -1 && typeof addItem !== "undefined") { // Need to exclude undefined because one parameter (either addItem or subtractItem) is likely left blank in the original function call
+      hideTheseAnswersArray.push(addItem);
+      console.log("add an item: " + addItem);
+      console.log(addItem);
+    } else {
+        console.log("There's already an " + addItem + " here. Don't do anything");
+    }
+  }
+  addToHideTheseAnswersArray(addItem);
+
+  function subtractFromHideTheseAnswersArray(subtractItem) {
+    if (hideTheseAnswersArray.indexOf(subtractItem) > -1 && typeof subtractItem !== "undefined") {
+      var arrayIndex = hideTheseAnswersArray.indexOf(subtractItem);
+      hideTheseAnswersArray.splice(arrayIndex, 1);
+      console.log("subtract:" + subtractItem);
+      console.log(subtractItem);
+      }
+  }
+  subtractFromHideTheseAnswersArray(subtractItem);
+}
+
+function updateStepVisiblity() {
+  if (hideTheseAnswersArray.includes("ticketDate")) {
+    ticketDate.style.display = "none";
+    console.log("I found ticketDate in the array and will hide the step");
+
+  }
+}
+
+const parkingViolationMULTIRadioAnswer = (function checkRadioAnswer() {
+
+  for(let i = 0; i < inputs.length; i++) {
+      inputs[i].addEventListener("click", checkRadioAnswer, false);
+  }
+
+  var radios = document.getElementsByName("violationType")
+
+  for (let i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      if (radios[i].value === "1") {
+        ticketDate.style.display = "none";
+        insertText("1");
+        return; // to stop from going through entire loop
+      }
+      else if (radios[i].value === "2") {
+        ticketDate.style.display = "none";
+        insertText("2");
+        return;
+      }
+      else if (radios[i].value === "3") { // need to use the specific section id instead of i
+        updateHideTheseAnswersArray(ticketDate, undefined);
+        insertText("3");
+        return
+      }
+      else if (radios[i].value === "4") {
+        updateHideTheseAnswersArray(undefined, ticketDate);
+        insertText("4");
+        return;
+      }
+    }
+
+  }
 }());
 
-// To add whitespace to the end of the document so each section div will scroll to the top of the window when Next button selected 
+// To add whitespace to the end of the document so each section div will scroll to the top of the window when Next button selected
 const setWhiteSpaceAtEndOfDocument = (function calcAndSetWhiteSpace() {
-  var lastDivHeight = document.getElementById("final-section-container").offsetHeight;
+  var lastDivHeight = finalSectionDiv.offsetHeight;
   var headerHeight = document.getElementById("header-main").offsetHeight;
   var footerHeight = document.getElementById("footer-main").offsetHeight;
   var newPadding = (window.innerHeight - lastDivHeight - headerHeight - footerHeight);
@@ -169,9 +276,14 @@ const setWhiteSpaceAtEndOfDocument = (function calcAndSetWhiteSpace() {
 
 
 
+// testing
+function testing2() {
+  document.getElementById("header-main").style.backgroundColor="red";
+};
 
-
-
+function testing3() {
+  document.getElementById("header-main").style.backgroundColor="green";
+};
 
 
 /*
