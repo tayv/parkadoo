f379d986d1a545f9b0a7e5abe0019c8d109118a0
Taylor - air <taylor@Taylors-MacBook-Air.local>
2019-11-03 11:39:09 -0700

    Moved formSections obj to generic-form.js module

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 import {setLetterTemplate, letterTemplate} from "/letter.js";
 import {checkBylawsOutputTemplate, cityBylawLink, cityBylawName, bylawTextObj} from "/bylaw.js";
 import {calcAndSetWhiteSpace, currentDateFormatted, upperCaseFirstLetter, lowerCaseFirstLetter, formatSentenceEnding} from "/helper-functions.js";
+import {formSections} from "/generic-form.js";
+
 // TO LOAD DEFAULT VISIBILITY CONDITIONS (works)
 window.onload = function setDefaultAnswerState() {
   document.getElementById("parking-form-content").reset();
@@ -16,41 +18,7 @@ window.onload = function setDefaultAnswerState() {
 let hideTheseSectionsArray = [];
   // Array that will hold section to display
 let showTheseSectionsArray = [];
-  // Holds the total number of sections
-const stepsQuestionnaire = document.getElementsByClassName("section-container");
 
-  // Sections
-const formSections = {
-  welcomeSection: document.getElementById("welcome-section"),
-  parkingProblemSection: document.getElementById("parking-problem-section"),
-  // If appealing ticket
-  parkingTicketIssuerSection: document.getElementById("ticket-issuer-section"),
-  // If appealing city ticket
-  municipalitySection: document.getElementById("municipality-section"),
-  cityUnavailableSection: document.getElementById("city-unavailable-section"),
-  ticketNumberSection: document.getElementById("ticket-number-section"),
-  ticketAccuracySection: document.getElementById("ticket-accuracy-section"),
-  ticketErrorDescriptionSubSection: document.getElementById("ticket-error-description-subsection"),
-  ticketReasonSection: document.getElementById("ticket-reason-section"),
-  ticketReasonOtherSubSection: document.getElementById("ticket-reason-other-subsection"),
-  ticketAppealBylawSection: document.getElementById("ticket-appeal-bylaw-section"),
-  ticketAppealSubSection: document.getElementById("ticket-appeal-bylaw-subsection"),
-  photoUploadSection: document.getElementById("photo-upload-section"),
-  photoUploadPromptSubSection: document.getElementById("photo-upload-prompt-subsection"),
-  ticketDateSection: document.getElementById("ticket-date-section"),
-  nameSection: document.getElementById("name-section"),
-  contactDetailsSection: document.getElementById("contact-details-section"),
-  mailingAddressSection: document.getElementById("mailing-address-section"),
-  // If appealing university ticket
-  studentOrEmployeeSection: document.getElementById("student-or-employee-section"),
-  // private operator and institution
-  privateTicketAppealSection: document.getElementById("private-ticket-appeal-section"),
-  // If checking bylaws
-  potentialIssueSection: document.getElementById("potential-issue-section"),
-  checkBylawsSection: document.getElementById("check-bylaw-info-section"),
-  // last step
-  finishedSectionDiv: document.getElementById("finished-section-container")
-};
 
 // Output
 let templateType = ""; // to be used as parameter for setLetterTemplate() to update the output template on next step button click
@@ -59,8 +27,8 @@ let yesStudentOrEmployee = "";
 
 // To hide all steps other than initial welcome section by default
 const hideAllSteps = function() {
-  for (var i = 1; i < stepsQuestionnaire.length; i++) {
-    stepsQuestionnaire[i].style.display="none";
+  for (var i = 1; i < formSections.allFormSections.length; i++) {
+    formSections.allFormSections[i].style.display="none";
   }
   formSections.finishedSectionDiv.style.display="none";
 };
