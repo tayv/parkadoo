d5c194dcadb971b060774b68b66a33e88249606e
Taylor - air <taylor@Taylors-MacBook-Air.local>
2020-01-04 17:08:04 -0700

    Copy function works but keeps background color

--- letter-actions.js
+++ letter-actions.js
@@ -7,13 +7,20 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("insert-letter-output-text-here").innerHTML = JSON.parse(sessionStorage.getItem("letter-output"));
   }, false);
 
-/*
-function checkIT () {
-  if (window.location.href.match("letter.html")) {
-    document.getElementById("parking-form-content").removeEventListener("click", clickActiveClass);
+// CTA Actions
+function copyDivToClipboard() {
+  var range = document.createRange();
+  range.selectNode(document.getElementById("insert-letter-output-text-here"));
+  window.getSelection().removeAllRanges(); // clear current selection
+  window.getSelection().addRange(range); // to select text
+  document.execCommand("copy");
+    window.getSelection().style.color="red";
+  window.getSelection().removeAllRanges(); // to deselect
+} */
+
+if (document.getElementById("button-copy")) {
+  document.getElementById("button-copy").onclick = () => {
+    copyDivToClipboard();
+    alert("copied");
   }
 }
-  console.log(templateType);
-  if (templateType == "report abandoned vehicle") {
-    document.querySelector("p").textContent = "hi";
-  } */
