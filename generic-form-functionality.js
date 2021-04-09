e88afcf278308bd3bfebcbe4643fd6fd96da3cfa
Taylor - air <taylor@Taylors-MacBook-Air.local>
2021-04-08 22:36:06 -0600

    removed old sessionstorage code

--- generic-form-functionality.js
+++ generic-form-functionality.js
@@ -203,27 +203,6 @@ const formAction = (str) => {
   }
 }
 
-/*
-// Using sessionStorage to save user answers
-document.getElementById("button-submit").onclick = () => {
-  try {
-      let storage = window.sessionStorage || {};
-      // Sanitize user input data
-      let templateDataDirty = setLetterTemplate(templateType); // So that we display the correct letter with up to date variables in letter.html
-      let templateDataClean = DOMPurify.sanitize(templateDataDirty);
-      // Store data
-      sessionStorage.setItem("letter-output", JSON.stringify(templateDataClean));
-      // Retrieving data done in letter.html header script on page load
-
-  } catch (e) {
-      let storage = {};
-      // Chrome doesn't allow sessStorage when 3rd party cookies are blocked.
-      alert(e.message, "Sorry, looks like I'm blocked from saving and displaying your answers because your browser doesn't allow 3rd party cookies in your advanced browser settings.");
-  }
-
-  formAction(templateType); // To send to right html page
-};
-*/
 
 // GENERIC FUNCTIONALITY: Add event listener to radio buttons within visibility condition function
 const addRadioEventListener = (rbClassName, updateConditionalsFunction) => {
